11c6de1570d23359251472a83e8a8ff453a790ed
Dono <erik.dono@gmail.com>
2020-07-26 17:34:17 -0400

    router working now

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+// import API from "../utils/API";
+// const repos =[]
+
+
+export class Portfilio extends Component {
+
+// was going to map through repos after filling it with the ajax call. 
+    // loadNextDog = () => {
+    //     API.getRepos()
+    //         .then(
+    //             repos = res.
+    //             )
+    //         )
+    //         .catch(err => console.log(err));
+// }
+    
+
+    render() {
+        return (
+            <card>
+            <div>
+             <p>this is it!</p>
+            </div>
+            </card>
+        );
+    }
+
+}
+export default Portfilio;
