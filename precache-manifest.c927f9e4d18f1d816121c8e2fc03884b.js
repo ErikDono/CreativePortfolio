2df33f7f182b4a7e76926602ac78d1e0d8c86658
Dono <erik.dono@gmail.com>
2020-08-03 22:43:38 -0400

    Updates

---
+++ precache-manifest.c927f9e4d18f1d816121c8e2fc03884b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e465e0ca8fc9170b4456c8e2742601d",
+    "url": "/CreativePortfolio/index.html"
+  },
+  {
+    "revision": "de43d31547e56f0fbc66",
+    "url": "/CreativePortfolio/static/css/main.f27e6f38.chunk.css"
+  },
+  {
+    "revision": "c5ae2cc08bcf746e6f56",
+    "url": "/CreativePortfolio/static/js/2.66803505.chunk.js"
+  },
+  {
+    "revision": "0929649d0b106c81fbc0aadc259880f6",
+    "url": "/CreativePortfolio/static/js/2.66803505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de43d31547e56f0fbc66",
+    "url": "/CreativePortfolio/static/js/main.8c804d79.chunk.js"
+  },
+  {
+    "revision": "9d51879a5517b51ff44d",
+    "url": "/CreativePortfolio/static/js/runtime-main.7d765ee2.js"
+  },
+  {
+    "revision": "2e7ebcc6344d691b8217a3586cd5474a",
+    "url": "/CreativePortfolio/static/media/Burger.2e7ebcc6.png"
+  },
+  {
+    "revision": "4ab343150e15fc4aa4de678852569ee9",
+    "url": "/CreativePortfolio/static/media/EandB.4ab34315.png"
+  },
+  {
+    "revision": "cf2a21a4e8ccd3284ae5ba4e05768e68",
+    "url": "/CreativePortfolio/static/media/EmpTrack.cf2a21a4.gif"
+  },
+  {
+    "revision": "9537697a32bb7bb7c1dd02c4414afcaf",
+    "url": "/CreativePortfolio/static/media/Fitness.9537697a.png"
+  },
+  {
+    "revision": "dd132a24afc94695c25fdee178640554",
+    "url": "/CreativePortfolio/static/media/SortEmp.dd132a24.png"
+  },
+  {
+    "revision": "cae55e5f2a80c731e0af2af0b72054be",
+    "url": "/CreativePortfolio/static/media/WorkProf.cae55e5f.gif"
+  },
+  {
+    "revision": "6e807578b910b3bac8a390ab025c5931",
+    "url": "/CreativePortfolio/static/media/covid.6e807578.png"
+  },
+  {
+    "revision": "925096873ca81022b1f8b326c0c5e4ec",
+    "url": "/CreativePortfolio/static/media/docImg.92509687.png"
+  },
+  {
+    "revision": "7ae8674aab2b586977b01378be56c441",
+    "url": "/CreativePortfolio/static/media/farm.7ae8674a.jpg"
+  },
+  {
+    "revision": "a7da6ac44b738374086cff7a48fb5076",
+    "url": "/CreativePortfolio/static/media/fbImg.a7da6ac4.png"
+  },
+  {
+    "revision": "427b8bc29df2dae700937053281fcaa7",
+    "url": "/CreativePortfolio/static/media/gitImg.427b8bc2.png"
+  },
+  {
+    "revision": "1a814bbf759dba5ffbbae74a74c40338",
+    "url": "/CreativePortfolio/static/media/passPic.1a814bbf.png"
+  },
+  {
+    "revision": "599eb2f1f6de933a04685c027de016c3",
+    "url": "/CreativePortfolio/static/media/resumeDoc.599eb2f1.pdf"
+  }
+]);
